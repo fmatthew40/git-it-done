df3187c84a21ead94fe9a086af3e40570254e48a
Matt Foster <fostmatt@Matts-MacBook-Pro.local>
2021-12-03 23:01:16 -0700

    update features

--- assets/java/homepage.js
+++ assets/java/homepage.js
@@ -2,6 +2,7 @@
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var getUserRepos = function(user) {
     // format the github api url
@@ -125,4 +126,32 @@ var displayRepos = function(repos, searchTerm) {
     }
   };
   
+var getFeaturedRepos = function(language)  {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      })
+    }else {
+      alert('Error: Github User Not Found');
+    }
+
+  });
+};
+
+var buttonClickHandler = function (event){
+  // get the language attribute from the clicked element
+  var language = event.target.getAttribute("data-language");
+
+if (language) {
+  getFeaturedRepos(language);
+  // clear old content
+  repoContainerEl.textContent = "";
+
+}
+};
+
+// getFeaturedRepos();
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
