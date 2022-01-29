1c04782d2d541514e4be9a158e1e873682f70572
Matt Foster <fmatthew40@gmail.com>
2022-01-29 08:18:20 -0700

    refactor js

--- assets/java/homepage.js
+++ assets/java/homepage.js
@@ -43,43 +43,6 @@ if (username) {
 }
 };
 
-// var displayRepos = function(repos, searchTerm) {
-//     repoContainerEl.textContent ="";
-//     repoSearchTerm.textContent = searchTerm;  
-
-//     // loop over repos
-//     for (var i=0; i< repos.length; i++)  {
-//         // format repo name
-//         var repoName = repos[i].owner.login + "/" + repos[i].name; 
-
-//         // create a container for each repo
-//         var repoEl = document.createElement("div");
-//         repoEl.classList = "list-item flex-row justify-space-between align-center";
-
-//         // create a span element to hold repository name
-//         var titleEl = document.createElement("span");
-//         titleEl.textContent = repoName;
-
-//         // append span to container - read backwards titleEl is being attached to repoEl
-//         repoEl.appendChild(titleEl);
-
-//         // create a status element
-//         var statusEl = document.createElement("span");
-//         statusEl.classList = "flex-row align-center";
-//         // check if current repo has issue or not
-//         if (repos[i].open_issues_count > 0) {
-//             statusEl.innerHTML =
-//             " <i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
-//         } else {
-//             statusEl.innerHTML = "<i class='fas fa-check-square status-icon"
-//         }
-
-
-//         // append container to the dom --- first one is where I want it attached
-//         repoContainerEl.appendChild(repoEl);
-//     }
-// };
-
 var displayRepos = function(repos, searchTerm) {
     // check if api returned any repos
     if (repos.length === 0) {
